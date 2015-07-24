4b6ae98bb61e99672029a067fc434be00749184f
vegdahl <vegdahl@up.edu>
2015-07-24 12:51:56 -0700

    Initial commit. Starter project is not completely finished yet.

---
+++ app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
@@ -0,0 +1,50 @@
+package edu.up.cs371.textmod;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class TextModActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_mod);
+
+
+        // Set up the spinner so that it shows the names in the spinner array resources
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        String[] spinnerNames = getResources().getStringArray(R.array.spinner_names);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, spinnerNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_text_mod, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
